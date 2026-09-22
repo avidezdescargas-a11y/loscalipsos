a087fc3c955887a4add067fb48000e34707a724c
avidezdescargas-a11y <avidezdescargas@gmail.com>
2026-09-22 13:01:12 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Copia aqui la configuracion WEB que muestra Firebase Console > Configuracion del proyecto.
+// La clave apiKey identifica tu proyecto; la seguridad depende de Authentication y Rules.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB7xj3CnNG8G1o2iQv60unUELKDgOJfP4w",
+  authDomain: "loscalipsosgps.firebaseapp.com",
+  databaseURL: "https://loscalipsosgps-default-rtdb.firebaseio.com",
+  projectId: "loscalipsosgps",
+  appId: "1:893766890025:web:9f76359cc1f65f1a1c2238"
+};
